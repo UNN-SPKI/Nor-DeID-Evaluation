b4e6d1521d0ba9f33a734a086260c77062e406b5
Jørgen Lund <jlu@dips.no>
2023-06-13 19:11:57 +0200

    Move messages to get_chat_completion to avoid caching previous questions

--- models/gpt_chat.py
+++ models/gpt_chat.py
@@ -17,7 +17,7 @@ CACHE_DIRECTORY = '.cache'
 
 SYSTEM_INSTRUCTION = """
 Annotate the following clinical note with XML-style tags.
-Enclose any strings that might be a name or acronym or initials, patients' names, doctors' names, the names of the M.D. or Dr. with a pair of <NAME> tags. 
+Enclose any strings that might be a name or acronym or initials, patients' names, doctors' names, the names of the M.D. or Dr. with a pair of <Name> tags. 
 Enclose any pager names and medical staff names with <Name> tags. 
 Enclose any strings that might be a location or address, such as \"Åssiden 31\" with <Location> tags. 
 Enclose the patient's ages and any strings that look like \"X år gammel\" with <Age> tags. 
@@ -58,21 +58,19 @@ def list_annotations(annotated: str) -> List[Tuple[int, int, str]]:
         annotations.append((tag_start, tag_end, tag_name))
     return annotations
 
-
-def construct_messages(source: str) -> List[dict]:
-    return [
-        {'role': 'system', 'content': SYSTEM_INSTRUCTION},
-        {'role': 'user', 'content': 'Input: ' + source}
-    ]
-
-
 def get_chat_completion(source, openAIAPIKey, temperature, rate_limit = None):
     if rate_limit:
         time.sleep(rate_limit)
+    prologue = [
+        {'role': 'system', 'content': SYSTEM_INSTRUCTION}
+    ]
+    messages = prologue.append({
+        'Input: ' + source
+    })
     r = requests.post('https://api.openai.com/v1/chat/completions',
                       json={
                           'model': 'gpt-3.5-turbo',
-                          'messages': construct_messages(source),
+                          'messages': messages,
                           'temperature': temperature
                       },
                       headers={
@@ -85,7 +83,9 @@ def get_chat_completion(source, openAIAPIKey, temperature, rate_limit = None):
     return response
 
 def fix_orthography(answer: str) -> str:
-    return re.sub('\s*([,.])\s*', r' \1 ', answer).rstrip()
+    space_punctuation = re.sub('\s*([,.])\s+', r' \1 ', answer).rstrip()
+    single_spaces = re.sub('\s+', ' ', space_punctuation)
+    return single_spaces
 
 class GptChatModel:
     def __init__(self, openAIAPIKey, rate_limit=2, retries=5):
@@ -100,6 +100,10 @@ class GptChatModel:
             logging.debug(f"Task: {doc.text}")
             prediction = self.predict_task(doc.text)
             logging.debug(f"Predicted: {prediction}")
+            if remove_tags(prediction) != doc.text.rstrip():
+                logging.warning("Misaligned text!")
+                logging.warning(f"ORIGINAL: {doc.text}")
+                logging.warning(f"RETURNED: {remove_tags(prediction)}")
             annotations = {'entities': list_annotations(prediction)}
             logging.debug(f"Annotations: {annotations}")
 
