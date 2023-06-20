c3935f6e17d39b72dd042033a93eecd3338adeb7
Jørgen Lund <jlu@dips.no>
2023-06-20 11:50:46 +0200

    Add wrapper for T5 models

---
+++ models/hf_t5.py
@@ -0,0 +1,114 @@
+"""
+hf_transformer is a wrapper around HuggingFace's Transformer library,
+loading a specified model which implements 
+(e.g. )
+"""
+
+import logging
+import re
+import time
+from typing import List, Tuple
+
+import spacy
+import transformers
+import accelerate
+
+ANNOTATION_PROMPT = """
+What are the first names in the following sentence?
+"""
+
+EXPECTED_TAGS = ['First_Name', 'Last_Name', 'Location', 'Health_Care_Unit', 'Age', 'Phone_Number', 'Social_Security_Number', 'Date']
+
+# _ENCLOSED_IN_TAGS matches on expressions with XML-style tags (e.g. '<Age>23</Age>')
+# putting the tag name in the first capturing group, and the contents in the second
+# capturing group.
+# NOTE: This will fail if you have nested annotations.
+_ENCLOSED_IN_TAGS = re.compile(r'<([\w_]*)>([^<]*)<\/\1>')
+
+def remove_tags(task: str) -> str:
+    return _ENCLOSED_IN_TAGS.sub(r'\2', task)
+
+def list_annotations(annotated: str) -> List[Tuple[int, int, str]]:
+    annotations = []
+    matches = _ENCLOSED_IN_TAGS.finditer(annotated)
+
+    # We want to find the character spans as they will be
+    # in the unannotated text. To achieve this, we keep a running
+    # count of how many markup characters have found so far
+    # in markup_offset:
+    markup_offset = 0
+    for match in matches:
+        tag_name, contents = match.groups()
+        # The annotations consist of an opening tag and a closing tag
+        # (e.g. <Age></Age>) - the tags themselves add 5 characters:
+        total_markup_chars = (2*len(tag_name) + 5)
+        tag_start = match.span()[0] - markup_offset
+        tag_end = match.span()[1] - markup_offset - total_markup_chars
+        markup_offset += total_markup_chars
+        if tag_name not in EXPECTED_TAGS:
+            continue
+        annotations.append((tag_start, tag_end, tag_name))
+    return annotations
+
+def fix_orthography(answer: str) -> str:
+    space_punctuation = re.sub('\s*([,.])\s+', r' \1 ', answer).rstrip()
+    single_spaces = re.sub('\s+', ' ', space_punctuation)
+    return single_spaces
+
+class HFT5Model:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        self.tokenizer = transformers.T5Tokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
+        config = transformers.AutoConfig.from_pretrained(model_name)
+        self.model = transformers.T5ForConditionalGeneration(config)
+        # with accelerate.init_empty_weights():
+        #    self.model = transformers.T5ForConditionalGeneration(config)
+        
+        # self.model.tie_weights()
+        # accelerate.load_checkpoint_and_dispatch(
+        #     self.model, model_name, device_map="auto", no_split_module_classes=["GPTJBlock"]
+        # )
+    
+    def predict(self, doc_bin: spacy.tokens.DocBin, language: spacy.Language) -> List[spacy.training.Example]:
+        examples = []
+        for doc in doc_bin.get_docs(language.vocab):
+            logging.debug(f"Task: {doc.text}")
+            start = time.time()
+            prediction = fix_orthography(self.predict_task(doc.text))
+            inference_time = time.time() - start
+            logging.debug(f"Finished in {inference_time} seconds.")
+
+            logging.debug(f"Predicted: {prediction}")
+            annotations = {'entities': list_annotations(prediction)}
+            logging.debug(f"Annotations: {annotations}")
+
+            example = spacy.training.Example.from_dict(doc, annotations)
+            examples.append(example)
+        return examples
+    
+    def predict_task(self, source: str) -> str:
+        ANNOTATION_TASK = f"""Nedenfor er en instruksjon som beskriver en oppgave, sammen med et input som gir ytterligere kontekst. Skriv et svar som fullfører forespørselen på riktig måte.
+
+### Instruksjon:
+{ANNOTATION_PROMPT}
+
+### Input:
+{source}
+
+### Respons:"""
+        inputs = self.tokenizer(ANNOTATION_TASK, return_tensors="pt")
+        input_ids = inputs["input_ids"]
+        generation_output = self.model.generate(
+            input_ids=input_ids,
+            generation_config=transformers.GenerationConfig(temperature=0.01, top_p=0.05, num_beams=1),
+            return_dict_in_generate=True,
+            output_scores=True,
+            max_new_tokens=256,
+            pad_token_id=self.tokenizer.eos_token_id
+        )
+        response = []
+        for seq in generation_output.sequences:
+            output = self.tokenizer.decode(seq, skip_special_tokens=True)
+            response.append(output.split("### Respons:")[-1].strip())
+        return ''.join(response)
+        
