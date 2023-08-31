47268339953e45dfe789f70d80ed00d5138dea2d
Jørgen Lund <jlu@dips.no>
2023-08-31 11:22:43 +0200

    Take prompt templates out of the method definitions

--- models/hf_transformer.py
+++ models/hf_transformer.py
@@ -16,25 +16,11 @@ import accelerate
 from models.utilities.alignment import fix_orthography
 from models.utilities.tags import list_annotations
 
-ANNOTATION_PROMPT = """
-Annotate the following clinical notes with XML-style tags.
-Enclose first names with <First_Name> tags. 
-Enclose last names with <Last_Name> tags.
-Enclose any strings that might be a location or address, such as "Åssiden 31" with <Location> tags. 
-Enclose clinical and hospital names with <Location> tags. 
-Enclose the patient's age and any texts that look like "X år gammel" with <Age> tags. 
-Enclose phone numbers with <Phone_Number> tags.
-Enclose 8 digit long numbers with <Phone_Number> tags. 
-Enclose social security numbers with <Social_Security_Number> tags.
-Enclose 11 digit long numbers with <Social_Security_Number> tags. 
-Enclose dates and times with <Date> tags.
-Do not use any tags which were not specified above.
-"""
-
 EXPECTED_TAGS = ['First_Name', 'Last_Name', 'Location', 'Health_Care_Unit', 'Age', 'Phone_Number', 'Social_Security_Number', 'Date']
 
 class HFTransformerModel:
-    def __init__(self, model_name: str):
+    def __init__(self, prompt: str, model_name: str):
+        self.prompt = prompt
         self.model_name = model_name
         self.tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
         config = transformers.AutoConfig.from_pretrained(model_name)
@@ -67,7 +53,7 @@ class HFTransformerModel:
         ANNOTATION_TASK = f"""Nedenfor er en instruksjon som beskriver en oppgave, sammen med et input som gir ytterligere kontekst. Skriv et svar som fullfører forespørselen på riktig måte.
 
 ### Instruksjon:
-{ANNOTATION_PROMPT}
+{self.prompt}
 
 ### Input:
 {source}
